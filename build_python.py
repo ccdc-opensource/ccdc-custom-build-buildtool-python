1b6eccce735abcf246203d77e0049cb4080025fa
Claudio Bantaloukas <cbantaloukas@ccdc.cam.ac.uk>
2020-05-10 15:06:02 +0100

    add path

--- build_python.py
+++ build_python.py
@@ -103,7 +103,7 @@ def install_pyenv_version(version):
     if linux():
         python_build_env['PATH']=f"/tmp/pyenvinst/plugins/python-build/bin:{python_build_env['PATH']}"
         
-    subprocess.run(f'sudo python-build {version} {python_version_destdir()}', shell=True, check=True, env=python_build_env)
+    subprocess.run(f'sudo env "PATH=$PATH" python-build {version} {python_version_destdir()}', shell=True, check=True, env=python_build_env)
         
 def output_archive_filename():
         return f'{output_base_name()}.tar.gz'
